f96c896f7695ded1991004a3891f0f828a397c5b
Antonina Kicha <antonina.kicha@gmail.com>
2023-01-09 23:29:58 +0200

    Добавляет список друзей

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
+
 
 export const App = () => {
   return (
@@ -15,6 +18,8 @@ export const App = () => {
   stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+
     </>
     
     
