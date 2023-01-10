442a86c1be090968d2bda170349a518c33d3f1b2
Antonina Kicha <antonina.kicha@gmail.com>
2023-01-10 23:06:16 +0200

    Добавляет Историю транзакций

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+
 
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
+
 
 
 export const App = () => {
@@ -19,6 +24,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
 
     </>
     
